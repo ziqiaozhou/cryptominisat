6e6c8eafbb25d3b9c8cbcf6ccecd14e1faef0fb9
ziqiaozhou <ziqiao@cs.unc.edu>
2017-03-14 07:30:05 -0400

    fix

--- src/scalmc.cpp
+++ src/scalmc.cpp
@@ -1175,20 +1175,20 @@ void CUSP::JaccardOneRoundFor3(uint64_t jaccardHashCount,JaccardResult* result ,
 		jaccardXorClause.clear();
 		jaccard_samples.clear();
 		//solver->simplify(&jaccardAssumps);
-		if((jaccard_vars.size()-jaccardHashCount)>4 || notSampled){	
+	//	if((jaccard_vars.size()-jaccardHashCount)>4 || notSampled){	
 			SetJaccardHash(jaccardHashCount,jaccardHashVars,jaccardAssumps,jaccardAssumps_lastZero,solver);
 			jaccardAssumps_two= jaccardAssumps_lastZero;
 			jaccardAssumps_two.pop_back();
 			jaccard3Assumps.push_back(jaccardAssumps);
 			jaccard3Assumps.push_back(jaccardAssumps_lastZero);
 			jaccard3Assumps.push_back(jaccardAssumps_two);
-		}else{
+	/*	}else{
 			jaccard3Assumps.push_back(jaccardAssumps);
 			jaccard3Assumps.push_back(jaccardAssumps_lastZero);
 			jaccard3Assumps.push_back(jaccardAssumps_two);
 			SetSampledJaccardHash(jaccardHashCount,jaccardHashVars,jaccard3Assumps,solver);
 			cout<<"sampled jaccard";
-		}
+		}*/
 		//	solver->simplify(&jaccardAssumps);
 		uint64_t hashPrev = LowerFib;
 		addKey2Map(jaccardHashCount,numHashList,numCountList,count);
