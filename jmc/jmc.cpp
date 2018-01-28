71ec805abd103490b11f2608ecd5ee6f012a5c69
ziqiaozhou <ziqiao@cs.unc.edu>
2018-01-28 06:18:43 -0500

    fix hash and count

--- jmc/jmc.cpp
+++ jmc/jmc.cpp
@@ -457,8 +457,8 @@ int JaccardMC::BoundedSATCount(unsigned maxSolutions, const vector<Lit> assumps,
 						lits.push_back(Lit(var, isTrue));
 						pushlit2Sols(sols, isTrue ? "1" : "0");
 						pushlit2Sols(fullsols, isTrue ? "1" : "0");
-					if (distribution.count(var)&& isTrue){
-						addw=distribution[var];
+						if (distribution.count(var)&& isTrue)
+							addw=distribution[var];
 					} else {
 						pushlit2Sols(sols, "*");
 						num_undef++;
