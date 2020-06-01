679fab92ebc188bea73a30b25ff8f8c3aa73b8f1
ziqiaozhou <ziqiao@cs.unc.edu>
2020-05-31 21:56:40 -0400

    fix

--- src/searcher.cpp
+++ src/searcher.cpp
@@ -1873,7 +1873,9 @@ bool Searcher::clean_clauses_if_needed()
     assert(decisionLevel() == 0);
     assert(qhead == trail.size());
     #endif
-
+    if (!ok) {
+        return ok = false;
+    }
     const size_t newZeroDepthAss = trail.size() - lastCleanZeroDepthAssigns;
     if (newZeroDepthAss > 0
         && simpDB_props < 0
